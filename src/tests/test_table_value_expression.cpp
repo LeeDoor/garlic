2cea2e07db55c0d4a361d81b6b9c7ee17a84b8af
leedoor <samoshenkoleonid@yandex.ru>
2026-02-14 09:24:07 +0300

    more_tests_for_table_expression

--- src/tests/test_table_value_expression.cpp
+++ src/tests/test_table_value_expression.cpp
@@ -8,12 +8,14 @@ class TestValueExpressionFixture : public ::testing::Test {
 public:
     TestValueExpressionFixture()
     : gatherer_{ std::make_shared<TableValueGathererMock>() }
+    , test_int_value_{ std::make_shared<CellIntValue>(5) }
     {}
 
 protected:
     using GathererPtr = std::shared_ptr<TableValueGathererMock>;
 
     GathererPtr gatherer_;
+    std::shared_ptr<CellIntValue> test_int_value_;
 };
 
 TEST_F(TestValueExpressionFixture, init) {
@@ -23,11 +25,19 @@ TEST_F(TestValueExpressionFixture, init) {
 TEST_F(TestValueExpressionFixture, getValue) {
     TableValueExpression expr("Name column");
 
-    auto return_value = std::make_shared<CellIntValue>(5);
     EXPECT_CALL(*gatherer_, get_table_value("Name column"))
-        .WillOnce(Return(return_value));
+        .WillOnce(Return(test_int_value_));
 
-    EXPECT_TRUE(expr.get_value(gatherer_)->equals(return_value));
+    EXPECT_TRUE(expr.get_value(gatherer_)->equals(test_int_value_));
+}
+
+TEST_F(TestValueExpressionFixture, thrownExceptionInGatherer_ShouldThrowToo) {
+    TableValueExpression expr("Name column");
+
+    EXPECT_CALL(*gatherer_, get_table_value("Name column"))
+        .WillOnce(::testing::Throw(std::logic_error("manually generated exception")));
+
+    EXPECT_THROW(expr.get_value(gatherer_), std::logic_error);
 }
 
 }
