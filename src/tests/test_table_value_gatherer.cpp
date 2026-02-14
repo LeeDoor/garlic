d1fa56fb5aba381d495a61d2395efb5a9ee42f76
leedoor <samoshenkoleonid@yandex.ru>
2026-02-14 07:55:55 +0300

    expanded tableValueGathererImpl tests

--- src/tests/test_table_value_gatherer.cpp
+++ src/tests/test_table_value_gatherer.cpp
@@ -50,9 +50,11 @@ TEST_F(TableValueGathererFixture, accessingData_0ByDefault) {
     GathererPtr tvg = std::make_shared<TableValueGathererImpl>(table_);
 
     CellValuePtr cellstr = tvg->get_table_value("String field");
+    CellValuePtr cellfloat = tvg->get_table_value("Float field");
     CellValuePtr cellint = tvg->get_table_value("Int field");
 
     EXPECT_TRUE(cellstr->equals(std::make_shared<CellStringValue>(str_Aboba10)));
+    EXPECT_TRUE(cellfloat->equals(std::make_shared<CellFloatValue>(1.6f)));
     EXPECT_TRUE(cellint->equals(std::make_shared<CellIntValue>(1)));
 }
 
@@ -61,9 +63,11 @@ TEST_F(TableValueGathererFixture, accessingData_rowSelect) {
     tvg->set_row_number(1);
 
     CellValuePtr cellstr = tvg->get_table_value("String field");
+    CellValuePtr cellfloat = tvg->get_table_value("Float field");
     CellValuePtr cellint = tvg->get_table_value("Int field");
 
     EXPECT_TRUE(cellstr->equals(std::make_shared<CellStringValue>(str_TEST2026)));
+    EXPECT_TRUE(cellfloat->equals(std::make_shared<CellFloatValue>(1e10f + 5)));
     EXPECT_TRUE(cellint->equals(std::make_shared<CellIntValue>(INT_MAX - 2024)));
 }
 
@@ -72,8 +76,17 @@ TEST_F(TableValueGathererFixture, wrongRowNumber_shouldThrowLogic) {
     tvg->set_row_number(3);
 
     EXPECT_THROW(tvg->get_table_value("String field"), std::logic_error);
+    EXPECT_THROW(tvg->get_table_value("Float field"), std::logic_error);
     EXPECT_THROW(tvg->get_table_value("Int field"), std::logic_error);
 }
 
+TEST_F(TableValueGathererFixture, misspellColumnName_throwLogic) {
+    GathererPtr tvg = std::make_shared<TableValueGathererImpl>(table_);
+    tvg->set_row_number(0);
+
+    EXPECT_THROW(tvg->get_table_value("sTRING FIELD"), std::logic_error);
+    EXPECT_THROW(tvg->get_table_value("afaejpffield"), std::logic_error);
+    EXPECT_THROW(tvg->get_table_value("Int field@@@"), std::logic_error);
 }
 
+}
