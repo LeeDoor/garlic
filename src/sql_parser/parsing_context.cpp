653703d393056a780794bf8637eeab845b37b738
leedoor <samoshenkoleonid@yandex.ru>
2026-04-06 13:33:45 +0300

    Revert "revert parse() to uptr"
    This reverts commit 3990adb73671f5426b232da4e1eae1e5c4675d03.

--- src/sql_parser/parsing_context.cpp
+++ src/sql_parser/parsing_context.cpp
@@ -14,6 +14,11 @@ void ParsingContext::reset_before_parse() {
     stored_error_ = std::nullopt;
 }
 
+
+ParsingContext::ParserPtr ParsingContext::create_parser() {
+    return ParserPtr(new yy::parser(*this), std::move(deleter));
+}
+
 ParsingContext::ParsingResult ParsingContext::parse(StringViewType query_string) {
     reset_before_parse();
     yy::parser parse(*this);
