f336abd80ed76ef24b5ea85af018d4fc0c657723
leedoor <samoshenkoleonid@yandex.ru>
2026-04-11 19:59:23 +0300

    make_STRING unit tests

---
+++ src/tests/unit/test_sql_lexer_make_string.cpp
@@ -0,0 +1,54 @@
+#include "parser.tab.hpp"
+#include "position.hpp"
+
+yy::parser::symbol_type make_STRING(std::string& s, garlic::sql_parser::Position& curloc);
+
+namespace garlic::sql_parser {
+
+static std::string extract_string_token_value(yy::parser::symbol_type& symbol) {
+    EXPECT_EQ(symbol.kind(), yy::parser::symbol_kind::S_STRING);
+    return symbol.value.template as<StringType>();
+}
+
+TEST(test_sql_lexer_make_string, singleQuotedLiteralShouldUnquoteAndClearSourceBuffer) {
+    std::string raw = "'hello_world'";
+    Position pos;
+
+    auto symbol = make_STRING(raw, pos);
+    EXPECT_EQ(extract_string_token_value(symbol), "hello_world");
+    EXPECT_TRUE(raw.empty());
+}
+
+TEST(test_sql_lexer_make_string, doubleQuotedLiteralShouldUnquoteAndKeepInnerSingleQuotes) {
+    std::string raw = "\"ab'cd\"";
+    Position pos;
+
+    auto symbol = make_STRING(raw, pos);
+    EXPECT_EQ(extract_string_token_value(symbol), "ab'cd");
+}
+
+TEST(test_sql_lexer_make_string, knownEscapesShouldBeDecoded) {
+    std::string raw = "'line1\\nline2\\t\\\\\\'\\\"'";
+    Position pos;
+
+    auto symbol = make_STRING(raw, pos);
+    EXPECT_EQ(extract_string_token_value(symbol), "line1\nline2\t\\'\"");
+}
+
+TEST(test_sql_lexer_make_string, unknownEscapesShouldStayVerbatim) {
+    std::string raw = "'a\\0b\\rc'";
+    Position pos;
+
+    auto symbol = make_STRING(raw, pos);
+    EXPECT_EQ(extract_string_token_value(symbol), "a\\0b\\rc");
+}
+
+TEST(test_sql_lexer_make_string, emptyQuotedStringShouldProduceEmptyValue) {
+    std::string raw = "''";
+    Position pos;
+
+    auto symbol = make_STRING(raw, pos);
+    EXPECT_EQ(extract_string_token_value(symbol), "");
+}
+
+} // namespace garlic::sql_parser
