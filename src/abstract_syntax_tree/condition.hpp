671d0ede27d8050758f84d3343c627f77975849f
leedoor <samoshenkoleonid@yandex.ru>
2026-04-19 08:43:18 +0300

    not removing condition but making it Expression`s child

--- src/abstract_syntax_tree/condition.hpp
+++ src/abstract_syntax_tree/condition.hpp
@@ -1,21 +1,29 @@
 #pragma once
+#include "cell_boolean_value.hpp"
+#include "expression.hpp"
 #include "table_value_gatherer.hpp"
-#include "validateable.hpp"
-#include "expected_cell_value.hpp"
 
 namespace garlic {
 
+class CellBooleanValue;
+
 /// Base class for all Conditions in query.
 /*! Condition is a logical entity that can be resolved as True or False. */
-class Condition : public CanBeValidated {
+class Condition : public Expression {
 public:
     virtual ~Condition() = default;
 
-    Condition(TypeRules::TypeOrError toe) : CanBeValidated{ toe } {}
+    Condition(TypeRules::TypeOrError toe) : Expression{ toe } {}
+
+    using ExpectedCellBooleanValue = std::expected<sptr<CellBooleanValue>, UnexpectedCellValue>;
+
+    virtual ExpectedCellBooleanValue resolve_bool(sptr<TableValueGatherer> gatherer) const = 0;
 
     /// Performs logical operations on underlying objects.
     /*! @throws std::logic_error may throw if did not validate first. */
-    virtual ExpectedCellValue resolve(sptr<TableValueGatherer> gatherer) const = 0;
+    ExpectedCellValue resolve(sptr<TableValueGatherer> gatherer) const override {
+	return resolve_bool(gatherer);
+    }
 };
 
 }
