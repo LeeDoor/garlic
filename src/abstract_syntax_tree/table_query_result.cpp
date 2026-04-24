ac3797ae9775815bc9129f953a1983d37d14d4c3
leedoor <samoshenkoleonid@yandex.ru>
2026-04-24 11:11:23 +0300

    printing table with nice formatting

--- src/abstract_syntax_tree/table_query_result.cpp
+++ src/abstract_syntax_tree/table_query_result.cpp
@@ -7,24 +7,56 @@ TableQueryResult::TableQueryResult(Table&& table)
 {}
 
 StringType TableQueryResult::form_table_result(Table&& table) {
-    StringType result_str;
-    bool is_first = true;
-    for(const Row& row : table) {
-	for(const StringType& cell : row) {
-	    if(!is_first) {
-		result_str += "\t";
-	    } is_first = false;
-	    result_str += cell;
-	}
-	result_str += "\n";
-	is_first = true;
-    }
-    result_str += "\n";
-    return result_str;
+    std::stringstream ss;
+    print_table(ss, table);
+    return ss.str();
 }
 
 StringViewType TableQueryResult::format() const {
     return table_result_;
 }
+
+void TableQueryResult::print_table(std::ostream& out, const Table& table) {
+    if (table.empty()) { return; }
+    auto widths = count_width_foreach_column(table);
+    print_horizontal_delimeter(out, widths);
+    for (std::size_t r = 0; r < table.size(); ++r) {
+        out << V_BAR;
+        for (std::size_t c = 0; c < table[0].size(); ++c) {
+            out << SPACE
+                << std::left
+                << std::setw(static_cast<int>(widths[c]))
+                << table[r][c]
+                << SPACE
+                << V_BAR;
+        }
+        out << std::endl;
+        if (r == 0 && table.size() > 1) {
+            print_horizontal_delimeter(out, widths);
+        }
+    }
+
+    print_horizontal_delimeter(out, widths);
+}
+std::vector<size_t> TableQueryResult::count_width_foreach_column(const Table& table) {
+    const std::size_t cols = table[0].size();
+    std::vector<std::size_t> widths(cols, 0);
+    for (const auto& row : table) {
+        for (std::size_t i = 0; i < cols; ++i) {
+            widths[i] = std::max(widths[i], row[i].size());
+        }
+    }
+    return widths;
+}
+
+void TableQueryResult::print_horizontal_delimeter(std::ostream& out, const std::vector<std::size_t>& widths) {
+    out << CROSS;
+    for (std::size_t i = 0; i < widths.size(); ++i) {
+        out << std::string(widths[i] + 2, H_BAR)
+	    << CROSS;
+    }
+    out << '\n';
+}
+
 
 }
--- src/abstract_syntax_tree/table_query_result.hpp
+++ src/abstract_syntax_tree/table_query_result.hpp
@@ -13,6 +13,14 @@ public:
     
 private:
     static StringType form_table_result(Table&& table);
+    static void print_table(std::ostream& out, const Table& table);
+    static void print_horizontal_delimeter(std::ostream& out, const std::vector<size_t>& widths);
+    static std::vector<size_t> count_width_foreach_column(const Table& table);
+
+    static constexpr char CROSS = '+';
+    static constexpr char V_BAR = '|';
+    static constexpr char H_BAR = '-';
+    static constexpr char SPACE = ' ';
 
     StringType table_result_;
 };
