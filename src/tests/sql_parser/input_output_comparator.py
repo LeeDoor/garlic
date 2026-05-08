23ff7e4ade55bd42af2e2c1d49ad9374f3adc237
leedoor <samoshenkoleonid@yandex.ru>
2026-05-08 09:52:25 +0300

    moving tests to different directories

--- src/tests/sql_parser/input_output_comparator.py
+++ src/tests/sql_parser/input_output_comparator.py
@@ -3,5 +3,5 @@ from pathlib import Path
 import runpy
 
 if __name__ == "__main__":
-    new_runner = Path(__file__).parent / "select_literal_tests" / "input_output_comparator.py"
+    new_runner = Path(__file__).parent / "select_query" / "select_only" / "input_output_comparator.py"
     runpy.run_path(str(new_runner), run_name="__main__")
