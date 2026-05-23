fc2cd9b1dbd8770b285eefaac22fc208de27a3e3
leedoor <samoshenkoleonid@yandex.ru>
2026-05-23 20:19:02 +0300

    comment to CellValueGatherer

--- src/abstract_syntax_tree/cell_value_gatherer.hpp
+++ src/abstract_syntax_tree/cell_value_gatherer.hpp
@@ -9,8 +9,9 @@ public:
     virtual ~CellValueGatherer() = default;
 
     virtual bool is_table_empty() const = 0;
+    /// Gets value for given column_name. To change pointer, use @ref jump_to_next_row .
     virtual sptr<CellValue> get_table_value(const ColumnNameType& column_name) = 0;
-    /// moves row pointer to next row. If overflowed, pointer is reset to 0 and true returned.
+    /// Moves row pointer to next row. If overflowed, pointer is reset to 0 and true returned.
     virtual bool jump_to_next_row() = 0;
 };
 
--- src/abstract_syntax_tree/cell_value_gatherer_impl.cpp
+++ src/abstract_syntax_tree/cell_value_gatherer_impl.cpp
@@ -7,8 +7,8 @@
 namespace garlic {
 
 CellValueGathererImpl::CellValueGathererImpl(sptr<TypedTable> table)
-: table_{ table }
-, row_number_{ 0 }
+    : table_{ table }
+    , row_number_{ 0 }
 {}
 
 bool CellValueGathererImpl::is_table_empty() const {
@@ -18,31 +18,31 @@ bool CellValueGathererImpl::is_table_empty() const {
 sptr<CellValue> CellValueGathererImpl::get_table_value(const ColumnNameType& column_name) {
     auto column_number = table_->get_column_number_by_name(column_name);
     if(!column_number)
-	throw std::logic_error("called invalid CellValueGathererImpl::get_table_value: no such column \"" + column_name + "\"");
+        throw std::logic_error("called invalid CellValueGathererImpl::get_table_value: no such column \"" + column_name + "\"");
     CellType type = table_->get_column_type(*column_number);
     switch(type) {
-    case String:
-	return std::make_shared<CellStringViewValue>(
-	    table_->get_value<StringType>(row_number_, *column_number)
-	);
-    case Int:
-	return std::make_shared<CellIntValue>(
-	    table_->get_value<IntType>(row_number_, *column_number)
-	);
-    case Float:
-	return std::make_shared<CellFloatValue>(
-	    table_->get_value<FloatType>(row_number_, *column_number)
-	);
-    default:
-	std::unreachable();
+        case String:
+            return std::make_shared<CellStringViewValue>(
+                table_->get_value<StringType>(row_number_, *column_number)
+            );
+        case Int:
+            return std::make_shared<CellIntValue>(
+                table_->get_value<IntType>(row_number_, *column_number)
+            );
+        case Float:
+            return std::make_shared<CellFloatValue>(
+                table_->get_value<FloatType>(row_number_, *column_number)
+            );
+        default:
+            std::unreachable();
     }
 }
 
 bool CellValueGathererImpl::jump_to_next_row() {
     ++row_number_;
     if(table_->is_row_index_overflow(row_number_)) {
-	row_number_ = 0;
-	return true;
+        row_number_ = 0;
+        return true;
     }
     return false;
 }
